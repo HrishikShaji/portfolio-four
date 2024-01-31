20c0ce29717c5f6fb07863e125a8f64c1c7743fd
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-31 10:39:30 +0530

    hero added

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,3 +1,11 @@
 export const Hero = () => {
-	return <div></div>;
+	return (
+		<div className="p-10 pt-40 h-screen">
+			<h1 className="text-9xl font-semibold  relative">
+				I SIMPLY DESIGN COOL WEBSITES ,AND MY NAME IS HRISHIK
+			</h1>
+			<div className="h-[130px] w-[740px] mix-blend-difference rounded-full bg-white top-[285px] left-1 absolute" />
+			<div className="h-[130px] w-[600px] mix-blend-difference rounded-full bg-white top-[417px] right-[370px] absolute" />
+		</div>
+	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
-	return <main className="w-full h-full bg-white text-black"></main>;
+	return (
+		<main className="w-full h-full bg-white  text-black">
+			<Hero />
+		</main>
+	);
 }
