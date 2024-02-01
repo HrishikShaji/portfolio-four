67ede98e43a0f4fb62247ec3b7fe631d9513baea
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-01 11:56:31 +0530

    added features animation

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -2,6 +2,7 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Duru_Sans } from "next/font/google";
 import { useRef } from "react";
 
 export const Features = () => {
@@ -17,21 +18,47 @@ export const Features = () => {
       threeRef.current &&
       fourRef.current
     ) {
-      const animateOne = (event: MouseEvent) => {
-        console.log(event);
-        const rect = oneRef.current?.getBoundingClientRect();
-        if (oneRef.current) {
-          gsap.to(oneRef.current, { scaleX: 3, duration: 1 });
-        }
+      const rect = oneRef.current?.getBoundingClientRect();
+      const animateOne = () => {
+        gsap.to(oneRef.current, {
+          scaleX: 2,
+          transformOrigin: "left",
+          duration: 1,
+          backgroundColor: "black",
+        });
+        gsap.to(twoRef.current, { xPercent: 100, duration: 1 });
       };
 
-      const animateExit = (event: MouseEvent) => {
-        if (oneRef.current) {
-          gsap.to(oneRef.current, { scaleX: 1, duration: 1 });
-        }
+      const animateTwo = () => {
+        console.log("started");
+        gsap.to(twoRef.current, {
+          scaleX: 2,
+          transformOrigin: "left",
+          duration: 1,
+          backgroundColor: "black",
+        });
       };
+
+      const exitOne = () => {
+        gsap.to(oneRef.current, {
+          scaleX: 1,
+          duration: 1,
+          backgroundColor: "white",
+        });
+        gsap.to(twoRef.current, { xPercent: 0, duration: 1 });
+      };
+      const exitTwo = () => {
+        gsap.to(twoRef.current, {
+          scaleX: 1,
+          duration: 1,
+          backgroundColor: "white",
+        });
+      };
+
+      twoRef.current.addEventListener("mouseenter", animateTwo);
+      twoRef.current.addEventListener("mouseleave", exitTwo);
       oneRef.current.addEventListener("mouseenter", animateOne);
-      oneRef.current.addEventListener("mouseleave", animateExit);
+      oneRef.current.addEventListener("mouseleave", exitOne);
     }
   }, {});
 
@@ -39,9 +66,18 @@ export const Features = () => {
     <div className="w-full h-full py-10">
       <div className="flex w-full h-[50vh]">
         <div ref={oneRef} className="border-2 w-full border-black" />
-        <div ref={twoRef} className="border-2 w-full border-black" />
-        <div ref={threeRef} className="border-2 w-full border-black" />
-        <div ref={fourRef} className="border-2 w-full border-black" />
+        <div
+          ref={twoRef}
+          className="border-r-2 border-y-2 w-full border-black"
+        />
+        <div
+          ref={threeRef}
+          className="border-r-2 border-y-2 w-full border-black"
+        />
+        <div
+          ref={fourRef}
+          className="border-r-2 border-y-2  w-full border-black"
+        />
       </div>
     </div>
   );
