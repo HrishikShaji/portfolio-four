cf8c8ad409b01d99e1b7e0e38859453bc249dbb7
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-03 20:37:29 +0530

    horizontal continuos animation okay

--- app/components/ProjectsHeading.tsx
+++ app/components/ProjectsHeading.tsx
@@ -7,11 +7,13 @@ import { useRef } from "react";
 export const ProjectsHeading = () => {
 	const containerRef = useRef<HTMLDivElement>(null);
 	const targetRef = useRef<HTMLDivElement>(null);
+	const endRef = useRef<HTMLHeadingElement>(null);
 	useGSAP(() => {
-		if (containerRef.current && targetRef.current) {
+		if (containerRef.current && targetRef.current && endRef.current) {
 			const amountToScroll = targetRef.current.offsetWidth - window.innerWidth;
+			const textLength = amountToScroll - endRef.current.offsetWidth;
 
-			gsap.to(targetRef.current, {
+			const tween = gsap.to(targetRef.current, {
 				x: -amountToScroll,
 				duration: 2,
 				scrollTrigger: {
@@ -19,6 +21,17 @@ export const ProjectsHeading = () => {
 					pin: containerRef.current,
 					start: "top 20%",
 					end: "+=" + amountToScroll,
+					scrub: 1,
+				},
+			});
+
+			gsap.to(endRef.current, {
+				scale: 0,
+				scrollTrigger: {
+					containerAnimation: tween,
+					trigger: endRef.current,
+					start: "left 75%",
+					end: "left 100%",
 					scrub: 1,
 					markers: true,
 				},
@@ -31,7 +44,12 @@ export const ProjectsHeading = () => {
 				<h1 className="text-[400px]  whitespace-nowrap ">
 					THESE ARE MY PROJECTS.
 				</h1>
-				<h1 className="text-[400px] whitespace-nowrap text-red-500">END</h1>
+				<h1
+					ref={endRef}
+					className="text-[400px] whitespace-nowrap text-red-500"
+				>
+					END
+				</h1>
 			</div>
 		</div>
 	);
