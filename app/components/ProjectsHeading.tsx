d46ca048616575c5605064e47eb940bc9b36c7c0
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-03 16:36:36 +0530

    animation pin stable

--- app/components/ProjectsHeading.tsx
+++ app/components/ProjectsHeading.tsx
@@ -5,37 +5,34 @@ import gsap from "gsap";
 import { useRef } from "react";
 
 export const ProjectsHeading = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const endRef = useRef<HTMLHeadingElement>(null);
-  const targetRef = useRef<HTMLDivElement>(null);
-  useGSAP(() => {
-    if (containerRef.current && endRef.current && targetRef.current) {
-      const amountToScroll = targetRef.current.offsetWidth;
+	const containerRef = useRef<HTMLDivElement>(null);
+	const targetRef = useRef<HTMLDivElement>(null);
+	useGSAP(() => {
+		if (containerRef.current && targetRef.current) {
+			const amountToScroll = targetRef.current.offsetWidth - window.innerWidth;
 
-      gsap.to(targetRef.current, {
-        x: -amountToScroll,
-        duration: 2,
-        scrollTrigger: {
-          trigger: targetRef.current,
-          pin: containerRef.current,
-          start: "top 20%",
-          end: "+=" + amountToScroll,
-          scrub: 1,
-          markers: true,
-        },
-      });
-    }
-  }, {});
-  return (
-    <div ref={containerRef} className="overflow-x-scroll flex gap-10">
-      <div className="flex gap-10" ref={targetRef}>
-        <h1 className="text-[400px]  whitespace-nowrap ">
-          THESE ARE MY PROJECTS.
-        </h1>
-        <h1 className="text-[400px] text-red-500" ref={endRef}>
-          END
-        </h1>
-      </div>
-    </div>
-  );
+			gsap.to(targetRef.current, {
+				x: -amountToScroll,
+				duration: 2,
+				scrollTrigger: {
+					trigger: containerRef.current,
+					pin: containerRef.current,
+					start: "top 20%",
+					end: "+=" + amountToScroll,
+					scrub: 1,
+					markers: true,
+				},
+			});
+		}
+	}, {});
+	return (
+		<div ref={containerRef} className="overflow-x-scroll flex gap-10">
+			<div className="flex gap-10" ref={targetRef}>
+				<h1 className="text-[400px]  whitespace-nowrap ">
+					THESE ARE MY PROJECTS.
+				</h1>
+				<h1 className="text-[400px] whitespace-nowrap text-red-500">END</h1>
+			</div>
+		</div>
+	);
 };
